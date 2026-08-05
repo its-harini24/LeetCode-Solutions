fff075d2d6bb6c5edc6e575cf0a220c1d31671fc
Harini N <harinin.profile@gmail.com>
2026-08-05 14:09:29 +0530

    Time: 1 ms (23.46%), Space: 48.1 MB (74.93%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void reverseString(char[] s) {
+    int right=s.length-1;
+    int left=0;
+   while(left<right){
+     char temp = s[right];
+     s[right]=s[left];
+     s[left]=temp;
+     left++;
+     right--;
+    
+   }     
+    }
+}
