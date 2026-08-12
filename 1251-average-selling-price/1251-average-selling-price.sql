d575c5b479b9d6de672dcf6ebd6080e46dc85812
Harini N <harinin.profile@gmail.com>
2026-08-12 21:43:32 +0530

    Time: 972 ms (29.5%), Space: 0B (100%) - LeetHub

--- 1251-average-selling-price/1251-average-selling-price.sql
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -2,7 +2,7 @@
 SELECT
     p.product_id,
     ROUND(
-        COALESCE(SUM(p.price * u.units) / SUM(u.units), 0),
+       ifnull (SUM(p.price * u.units) / SUM(u.units), 0),
         2
     ) AS average_price
 FROM Prices p
